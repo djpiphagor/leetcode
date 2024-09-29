dd35a5087c7b1a041dca60ce53e7eb82da6b334f
Roman Zhukov <ruzhukov@outlook.com>
2024-09-29 15:36:51 +0300

    tribonacci

---
+++ tribonacci/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/n-th-tribonacci-number/
+
+func main() {
+	fmt.Println(tribonacci(4))
+	fmt.Println(tribonacci(25))
+	fmt.Println(fibonacci(4))
+	fmt.Println(fibonacci(25))
+}
+
+func tribonacci(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n == 1 || n == 2 {
+		return 1
+	}
+	a, b, c := 0, 1, 1
+	for i := 0; i < n-2; i++ {
+		a, b, c = b, c, a+b+c
+	}
+	return c
+}
+
+func fibonacci(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	a, b := 0, 1
+	for i := 0; i < n-1; i++ {
+		a, b = b, a+b
+	}
+	return b
+}
